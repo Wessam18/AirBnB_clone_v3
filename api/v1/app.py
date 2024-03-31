f6489ea74ca39c54a277c61eb1ee1fb210ab1147
wessam18 <wessamabdelnabi903@gmail.com>
2024-04-01 01:14:49 +0200

    edit pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,12 +7,11 @@ from api.v1.views import app_views
 from os import getenv
 
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 if __name__ == "__main__":
+    """ """
     Host1 = getenv('HBNB_API_HOST', '0.0.0.0')
     Port1 = int(getenv('HBNB_API_PORT', 5000))
     app.run(host=Host1, port=Port1, threaded=True)
-    
