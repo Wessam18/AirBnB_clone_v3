c7cc9af0ff1ca0fea1c19ad3d78e5fb5e18ce7e7
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-04-02 00:00:04 +0200

    a

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,10 +5,12 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
