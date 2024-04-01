0eef4ddb34234e36bb9c3548f02e9507622b9572
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-04-01 17:18:32 +0200

    add classes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,12 @@
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.user import User
+from models.review import Review
+from models.amenity import Amenity
 
 
 @app_views.route('/status')
@@ -16,12 +22,12 @@ def get_status():
 def get_count():
     """Endpoint to retrieve the number of each object type"""
     stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
 
-    return jsonify(stats)
+    return storage.count(stats)
