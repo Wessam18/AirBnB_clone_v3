c0a1205e79e2993bc06556deeab51a937d2b5230
wessam18 <wessamabdelnabi903@gmail.com>
2024-04-01 03:49:18 +0200

    task 4 count of each obj

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,24 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
 def get_status():
     """Route to return the status of the API"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/api/v1/stats')
+def get_count():
+    """Endpoint to retrieve the number of each object type"""
+    stats = {
+        "amenities": storage.count("Amenity"), 
+        "cities": storage.count("City"), 
+        "places": storage.count("Place"), 
+        "reviews": storage.count("Review"), 
+        "states": storage.count("State"), 
+        "users": storage.count("User")
+    }
+
+    return jsonify(stats)
