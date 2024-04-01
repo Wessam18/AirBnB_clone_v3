42628c8c12b1ea66c0b45392affaca253213984a
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-04-01 22:57:10 +0200

    strict_slaches:

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,13 +12,13 @@ from models.review import Review
 from models.amenity import Amenity
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def get_status():
     """Route to return the status of the API"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', strict_slashes=False)
 def get_count():
     """Endpoint to retrieve the number of each object type"""
     stats = {
@@ -27,7 +27,7 @@ def get_count():
         "places": storage.count(Place),
         "reviews": storage.count(Review),
         "states": storage.count(State),
-        "users": storage.count(User)
+        "users": storage.count(User),
     }
 
     return jsonify(stats)
